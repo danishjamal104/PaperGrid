0435d2d0914f033f6febf9e62b981fed29f95019
robustTechie <captain.shank75@gmail.com>
2020-03-05 02:45:26 +0530

    Bottom navigation update

---
+++ app/src/main/java/com/example/papergrid/ui/Inbox/InboxFragment.java
@@ -0,0 +1,36 @@
+package com.example.papergrid.ui.Inbox;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModel;
+import androidx.lifecycle.ViewModelProviders;
+
+import com.example.papergrid.R;
+
+public class InboxFragment extends Fragment {
+
+    private InboxViewModel InboxViewModel;
+
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
+        InboxViewModel =
+                ViewModelProviders.of(this).get(InboxViewModel.class);
+        View root = inflater.inflate(R.layout.fragment_inbox, container, false);
+        final TextView textView = root.findViewById(R.id.text_home);
+        InboxViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });
+        return root;
+    }
+}
---
+++ app/src/main/java/com/example/papergrid/ui/Inbox/InboxViewModel.java
@@ -0,0 +1,19 @@
+package com.example.papergrid.ui.Inbox;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class InboxViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public InboxViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is Inbox fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
